cfb5c45f41521fcdeefc56ec66b9461fe0345c70
Andrew Newdigate <andrew@troupe.co>
2014-01-08 18:11:26 +0200

    Handle repo issues

--- test/gitter.js
+++ test/gitter.js
@@ -1,20 +1,101 @@
+/*jslint node: true */
+/*global describe:true, it: true, beforeEach:true, afterEach:true */
+"use strict";
 
-var marked = require('../');
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var marked = require('..');
 
-function convert(text) {
-  var options = { gfm: true, tables: true, sanitize: true, breaks: true, linkify: true };
-
-  var renderer = new marked.Renderer();
-
-  var lexer = new marked.Lexer(options);
-
-  var tokens = lexer.lex(text);
-  console.log(tokens);
-
-  var parser = new marked.Parser(options, renderer);
-  return parser.parse(tokens);
+function getDefaultOptions() {
+  return { gfm: true, tables: true, sanitize: true, breaks: true, linkify: true };
 }
 
 
-console.log(convert("#12312 #1232 #3123 @moo\n \n\nhttp://gitter.com/ www.google.com andrewn@datatribe.net"));
-console.log(convert("```hello();```"));
+describe('issue', function() {
+  it('should read simple issues', function() {
+    var text = 'Deal with #123';
+    var options = getDefaultOptions();
+
+    var lexer = new marked.Lexer(options);
+    var issues = 0;
+    var renderer = new marked.Renderer();
+    renderer.issue = function(repo, issue, text) {
+      issues++;
+      assert(!repo);
+      assert.equal(issue, '123');
+      assert.equal(text, '#123');
+      return util.format('<a href="#" data-link-type="issue" data-issue="%s" class="issue">%s</a>', issue, text);
+    };
+
+    var tokens = lexer.lex(text);
+    options.renderer = renderer;
+
+    var parser = new marked.Parser(options);
+    var html = parser.parse(tokens);
+
+    assert.equal(issues, 1);
+    assert.equal(html, '<p>Deal with <a href="#" data-link-type="issue" data-issue="123" class="issue">#123</a></p>\n');
+  });
+
+  it('should read repo issues', function() {
+    var text = 'Deal with a/b#123';
+    var options = getDefaultOptions();
+
+    var lexer = new marked.Lexer(options);
+    var issues = 0;
+    var renderer = new marked.Renderer();
+    renderer.issue = function(repo, issue, text) {
+      issues++;
+      assert.equal(repo, 'a/b');
+      assert.equal(issue, '123');
+      assert.equal(text, 'a/b#123');
+      return util.format('<a href="#" data-link-type="issue" data-issue="%s" data-issue-repo="%s" class="issue">%s</a>', issue, repo, text);
+    };
+
+    var tokens = lexer.lex(text);
+    options.renderer = renderer;
+
+    var parser = new marked.Parser(options);
+    var html = parser.parse(tokens);
+
+    assert.equal(issues, 1);
+    assert.equal(html, '<p>Deal with <a href="#" data-link-type="issue" data-issue="123" data-issue-repo="a/b" class="issue">a/b#123</a></p>\n');
+  });
+
+
+  it('should read multiple repo issues', function() {
+    var text = 'Deal with a/b#123 #456 yeah';
+    var options = getDefaultOptions();
+
+    var lexer = new marked.Lexer(options);
+    var issues = 0;
+    var renderer = new marked.Renderer();
+    renderer.issue = function(repo, issue, text) {
+      issues++;
+      switch(issue) {
+        case '123':
+          assert.equal(repo, 'a/b');
+          assert.equal(text, 'a/b#123');
+          break;
+       case '456':
+         assert(!repo);
+         assert.equal(text, '#456');
+         break;
+
+      }
+      return util.format('<issue>%s</issue>', text);
+    };
+
+    var tokens = lexer.lex(text);
+    options.renderer = renderer;
+
+    var parser = new marked.Parser(options);
+    var html = parser.parse(tokens);
+
+    assert.equal(issues, 2);
+    assert.equal(html, '<p>Deal with <issue>a/b#123</issue> <issue>#456</issue> yeah</p>\n');
+  });
+
+});
---
+++ test/static.js
@@ -0,0 +1,46 @@
+/*jslint node: true */
+/*global describe:true, it: true, beforeEach:true, afterEach:true */
+"use strict";
+
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var marked = require('..');
+
+function runStaticTest(file) {
+  return function() {
+    var engine = marked;
+    var fileName = path.join(__dirname, '../tests', file);
+
+    var text = fs.readFileSync(fileName, 'utf8');
+    var html = fs.readFileSync(fileName.replace(/[^.]+$/, 'html'), 'utf8');
+
+    var generated = engine(text);
+    var generatedNoWhitespace = generated.replace(/\s/g, '');
+    var htmlNoWhitespace = html.replace(/\s/g, '');
+
+    assert.equal(generatedNoWhitespace, htmlNoWhitespace);
+  };
+}
+
+describe('static', function() {
+  var list = fs
+    .readdirSync(path.join(__dirname, '../tests'))
+    .filter(function(file) {
+      return path.extname(file) !== '.html';
+    })
+    .filter(function(file) {
+      // Ignore broken tests
+      return file != 'def_blocks.text' && file != 'double_link.text' && file != 'gfm_code_hr_list.text';
+    })
+    .sort(function(a, b) {
+      a = path.basename(a).toLowerCase().charCodeAt(0);
+      b = path.basename(b).toLowerCase().charCodeAt(0);
+      return a > b ? 1 : (a < b ? -1 : 0);
+    });
+
+  list.forEach(function(file) {
+    it(file, runStaticTest(file));
+  });
+
+});
